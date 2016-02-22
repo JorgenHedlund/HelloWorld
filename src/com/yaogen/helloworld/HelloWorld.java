045789b33974a425485e87c0bddb98aa7c11568a
Jorgen Hedlund <jorgen.hedlund@gmail.com>
2016-02-22 13:07:54 +0100

    Added .gitignore

---
+++ src/com/yaogen/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.yaogen.helloworld;
+
+/**
+ * Created by jorgen on 2016-02-22.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+    }
+}
